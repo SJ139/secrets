a42ebe94b247ffafcdc197e3d0c683385ed75cd9
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-08-21 16:35:43 -0400

    with basic encryption

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-require ('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -19,8 +19,8 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "Thisisourlittlesecret"
-userSchema.plugin(encryption, {secret: secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encryption, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
